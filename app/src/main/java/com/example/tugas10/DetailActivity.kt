35b7b2e233216e150a3fa051b90e0cf815f7c081
Syafiq Abdillah Habib <fiqabdillah8893@gmail.com>
2024-11-03 20:20:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas10/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tugas10
+
+import Mail
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val mail = intent.getParcelableExtra<Mail>("MAIL_DETAIL")
+
+        findViewById<TextView>(R.id.senderDetail).text = mail?.sender
+        findViewById<TextView>(R.id.dateDetail).text = mail?.date
+        findViewById<TextView>(R.id.contentDetail).text = mail?.content
+    }
+}
